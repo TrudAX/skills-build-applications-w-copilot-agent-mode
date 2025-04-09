52115f2b972101f63b50f9e23ef526e6a9bf9628
Denis Trunin <trud81@gmail.com>
2025-04-09 11:13:13 +0000

    Update styles in App.css

---
+++ octofit-tracker/frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import favicon from './favicon.ico';
+
+// Set favicon dynamically
+const link = document.createElement('link');
+link.rel = 'icon';
+link.href = favicon;
+document.head.appendChild(link);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
